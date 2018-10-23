6074a7431bc141597c928124ef453f6ffea21b7a
martik97 <martik97@gmail.com>
2018-10-23 15:56:04 +0200

    Pobieranie wejścia, początek metody isCorrect

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        String city, name, surname, number;
+
+        Scanner console = new Scanner(System.in);
+        city = console.nextLine();
+        System.out.println(city);
+
+        name = console.next();
+        System.out.println(name);
+
+        surname = console.next();
+        System.out.println(surname);
+
+        number = console.next();
+        System.out.println(number);
+
+        System.out.println(isCorrect(number));
     }
+
+    public static boolean isCorrect(String n) {
+        char[] pesel = new char [12];
+        pesel = n.toCharArray();
+        System.out.println(pesel[0]);
+        return true;
+      }
 }
