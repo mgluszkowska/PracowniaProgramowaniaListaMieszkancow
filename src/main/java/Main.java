4ea0a41da55b8c4e8703b18df4baad2c39367358
martik97 <martik97@gmail.com>
2018-10-24 14:40:33 +0200

    Poprawienie metody isCorrect, zapis peselu do pliku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
@@ -20,38 +22,36 @@ public class Main {
 
         System.out.println(isCorrect(number));
 
+        File myFile = new File("listamieszkancow.txt");
+        PrintWriter writeToFile = new PrintWriter("listamieszkancow.txt");
+        writeToFile.println(number);
     }
 
     public static boolean isCorrect(String n) {
-        int[] pesel = new int[13];
-        if (n.length()!=11) {
+        int[] pesel = new int[n.length()];
+        if (n.length() != 11) {
             return false;
-        }
-        else {
+        } else {
+            String[] stringArray = new String[13];
+            stringArray = n.split("");
             char[] arr = new char[12];
             arr = n.toCharArray();
-            System.out.println(2*arr[0]);
+
             int i;
-            //System.out.println(Integer.parseInt(n[0]));
-//            for (i=0; i<=11; i++){
-//                //if (java.lang.Character.isDigit(arr[i])==false) {
-//                  //  return false;
-//               // }
-//                //else {
-//                //pesel[i] = Integer.parseInt(arr[i]);
-//                //}
-//
-//            }
-//            int controlNumber = 9*pesel[0] + 7*pesel[1] + 3*pesel[2] + pesel[3] + 9*pesel[4] + 7*pesel[5] + 3*pesel[6] + pesel[7] + 9*pesel[8] + 7*pesel[9];
-//            if (controlNumber%10 == pesel[10]) {
-//                return true;
-//            }
-//            else {
-//                return false;
-//            }
-            return true;
+            for (i = 0; i < stringArray.length; i++) {
+                if (java.lang.Character.isDigit(arr[i]) == false) {
+                    return false;
+                } else {
+                    pesel[i] = Integer.parseInt(stringArray[i]);
+                }
+            }
+            int controlNumber = 9 * pesel[0] + 7 * pesel[1] + 3 * pesel[2] + pesel[3] + 9 * pesel[4] + 7 * pesel[5] + 3 * pesel[6] + pesel[7] + 9 * pesel[8] + 7 * pesel[9];
+            if (controlNumber % 10 == pesel[10]) {
+                return true;
+            } else {
+                return false;
+            }
         }
-      }
 
-
+    }
 }
