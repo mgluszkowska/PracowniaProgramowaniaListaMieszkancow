09ffeb653374b69dc7fb9c089152c38e14ae0013
martik97 <martik97@gmail.com>
2018-11-04 10:46:01 +0100

    Naprawa błędów odliczania czasu w weekend i w piątek.

--- src/main/java/classes/JobCountdown.java
+++ src/main/java/classes/JobCountdown.java
@@ -17,7 +17,7 @@ public class JobCountdown implements org.quartz.Job {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
 
-        LocalDateTime now = LocalDateTime.of(2018, 11, 5, 8, 0); // LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.of(2018, 11, 2, 7, 0); //LocalDateTime.now();
         LocalTime[] endDates = new LocalTime[12];
         endDates[0] = LocalTime.of(8, 15);
         endDates[1] = LocalTime.of(9, 45);
@@ -45,23 +45,33 @@ public class JobCountdown implements org.quartz.Job {
         if (now.getDayOfWeek().name().equals("FRIDAY")) {
             if (now.toLocalTime().isAfter(LocalTime.of(18, 45))) {
                 LocalTime endTime = LocalTime.of(8, 15);
-                LocalDateTime endDate = LocalDateTime.of(now().plusDays(2), endTime);
+                LocalDateTime endDate = LocalDateTime.of(now().plusDays(1), endTime);
                 minutes = Duration.between(now, endDate).toMinutes();
                 System.out.println(minutes + " minut do konca przerwy");
             }
             else {
-                System.out.println("Dzien pracy");
+                if (timeIndex %2 == 0) {
+                    LocalDateTime endDate = LocalDateTime.of(now.toLocalDate(), endDates[timeIndex]);
+                    minutes = Duration.between(now, endDate).toMinutes();
+                    System.out.println(minutes + " minut do konca przerwy");
+                }
+                else {
+                    LocalDateTime endDate = LocalDateTime.of(now.toLocalDate(), endDates[timeIndex]);
+                    minutes = Duration.between(now, endDate).toMinutes();
+                    System.out.println(minutes + " minut do konca zajec");
+                }
             }
         }
-        else if (now.getDayOfWeek().name().equals("SATURDAY")) {
+        else if (now.getDayOfWeek().name().equals("SATURDAY") && now.toLocalTime().isBefore(LocalTime.of(8, 15))) {
                 LocalTime endTime = LocalTime.of(8, 15);
-                LocalDateTime endDate = LocalDateTime.of(now().plusDays(2), endTime);
+                LocalDateTime endDate = LocalDateTime.of(now().plusDays(1), endTime);
                 minutes = Duration.between(now, endDate).toMinutes();
                 System.out.println(minutes + " minut do konca przerwy");
         }
-        else if (now.getDayOfWeek().name().equals("SUNDAY")) {
+        else if (now.getDayOfWeek().name().equals("SATURDAY") && now.toLocalTime().isAfter(LocalTime.of(8, 15))
+                || now.getDayOfWeek().name().equals("SUNDAY")) {
                 LocalTime endTime = LocalTime.of(8, 15);
-                LocalDateTime endDate = LocalDateTime.of(now.toLocalDate().plusDays(1), endTime);
+                LocalDateTime endDate = LocalDateTime.of(now().plusDays(1), endTime);
                 minutes = Duration.between(now, endDate).toMinutes();
                 System.out.println(minutes + " minut do konca przerwy");
         }
